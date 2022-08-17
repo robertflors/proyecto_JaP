a345634d132de8b77ce5c68860e59a4b3a87d1ca
Robert Flores <rrfloresgonzalez@gmail.com>
2022-08-17 20:47:34 -0300

    se agregó autenticación al index

--- js/autenticacion.js
+++ js/autenticacion.js
@@ -1,9 +1,12 @@
+let indexAutenticacion = document.getElementById("autenticacion");
+let indexLanding = document.getElementById("landingPage");
+
+let alertaEmail = document.getElementById('validacionEmail');
+let alertaContrasenia = document.getElementById('validacionContrasenia');
+
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("inicioSesion").addEventListener('submit', validarFormulario); 
   });
-
-  let alertaEmail = document.getElementById('validacionEmail');
-  let alertaContrasenia = document.getElementById('validacionContrasenia');
 
   function esconderAlertas(){
     document.getElementById('emailUsuario').addEventListener('onkeypress', () => alertaEmail.style.display = 'none');
@@ -12,12 +15,28 @@ document.addEventListener("DOMContentLoaded", function() {
   
   function validarFormulario(e) {
     e.preventDefault();
+    // dos booleanos que serán verdaderos si los datos están ingresados correctamente
+    // y de ser así se ocultará la autenticación y se mostrará el landing
+    let booleano1 = false;
+    let booleano2 = false;
+
+    // usamos el value de los diferentes inputs a ver si cumplen con los requisitos
     let email = document.getElementById('emailUsuario');    
     if(email.value.length == 0) {
       alertaEmail.style.display = 'inline-block';
+    } else {
+      booleano1 = true;
     }
+
     let contrasenia = document.getElementById('contraseniaUsuario');
     if (contrasenia.value.length < 6) {
         alertaContrasenia.style.display = 'inline-block';
+    } else {
+      booleano2 = true;
+    }
+    // si se cumplen las condiciones se muestra la página
+    if ((booleano1 == true)&&(booleano2 == true)){
+      indexAutenticacion.style.display = 'none';
+      indexLanding.style.display = 'contents';
     }
   }
--- js/init.js
+++ js/init.js
@@ -40,3 +40,5 @@ let getJSONData = function(url){
         return result;
     });
 }
+
+
