dd8cd2ee7faa588d3030233607509fe907062f6d
Robert Flores <rrfloresgonzalez@gmail.com>
2022-08-30 17:32:14 -0300

    se agregaron los diferentes filtros a productos, aún falta arreglar el problema asincrónico en el index y el usuario en navbar

--- js/products.js
+++ js/products.js
@@ -8,11 +8,13 @@ const PRODUCTOS_ID = localStorage.getItem("catID");
 const PRODUCTOS_URL = "https://japceibal.github.io/emercado-api/cats_products/" + PRODUCTOS_ID + ".json";
 
 // Función para mostrar la lista desplegada de productos de determinada categoría
-function showProductsList (productos) { 
-  
+function showProductsList(productos) {
+    // vaciamos el div que muestra los productos antes de llenarlo con la nueva info.
+    mostrarProductos.innerHTML = '';
+
     for (const producto of productos) {
         mostrarProductos.innerHTML += `
-            <div class="list-group-item list-group-item-action cursor-active">
+            <div class="list-group-item list-group-item-action cursor-active"> 
                 <div class="row">
                     <div class="col-3">
                         <img src="${producto.image}" alt="${producto.description}" class="img-thumbnail">
@@ -26,18 +28,82 @@ function showProductsList (productos) {
                     </div>
                 </div>
             </div>
-            `;   
-    }   
- }
-
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCTOS_URL).then(function(resultObj){
-        if (resultObj.status === "ok"){
-            categoriaSubtitulo.append(resultObj.data.catName); //para agregar el nombre al strong que hay en el subtítulo del products.html     
-            showProductsList(resultObj.data.products);            
-        }       
-       
-    });
+            `;
+    }
 }
-);
 
+document.addEventListener("DOMContentLoaded", function () {
+    getJSONData(PRODUCTOS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            categoriaSubtitulo.append(resultObj.data.catName); //para agregar el nombre al strong que hay en el subtítulo del products.html     
+            showProductsList(resultObj.data.products);
+            // seteamos en el local storage el arreglo que sacamos de la promesa para usarlo más adelante  
+            localStorage.removeItem('listaProductos');
+            localStorage.setItem('listaProductos', JSON.stringify(resultObj.data.products));
+            // seteo otro para preservar los datos "originales"
+            localStorage.removeItem('productos');
+            localStorage.setItem('productos', JSON.stringify(resultObj.data.products));
+        }
+    });
+});
+
+// ---------------------------------------FILTROS PARA PRODUCTOS--------------------------------------
+// ---------------------------------------------------------------------------------------------------
+
+const precioAsc = document.getElementById('sortAscPrecio');
+const precioDes = document.getElementById('sortDesPrecio');
+const relevancia = document.getElementById('sortRelevancia');
+const rangoPrecioMin = document.getElementById('filterRangoPrecioMin');
+const rangoPrecioMax = document.getElementById('filterRangoPrecioMax');
+const botonRangoPrecio = document.getElementById('filterRangoPrecio');
+const limpiarFiltroRango = document.getElementById('limpiarRangoPrecio');
+const barraBusqueda = document.getElementById('barraBusqueda');
+
+function ordenarProductos(e) {
+    // obtenemos el ID del input que está disparando el evento para luego hacer los condicionales
+    let orden = e.currentTarget.id;
+    // array que tendrá el valor de la lista de productos actual
+    let productoOrdenado = JSON.parse(localStorage.getItem('listaProductos'));
+    // los condicionales según el ID
+    if (orden === 'sortAscPrecio') {
+        showProductsList(productoOrdenado.sort((a, b) => a.cost - b.cost));
+    } else if (orden === 'sortDesPrecio') {
+        showProductsList(productoOrdenado.sort((a, b) => b.cost - a.cost));
+    } else if (orden === 'sortRelevancia') {
+        showProductsList(productoOrdenado.sort((a, b) => b.soldCount - a.soldCount));
+    }
+}
+
+function filtrarProductos() {
+    //creamos una variable que será el array de productos filtrados con los rangos de precios ingresados en los imputs
+    let productosFiltrados = JSON.parse(localStorage.getItem('productos')). //salto de línea para mejor lectura
+    filter(producto => producto.cost >= parseInt(rangoPrecioMin.value) && producto.cost <= parseInt(rangoPrecioMax.value));
+    showProductsList(productosFiltrados);
+    // seteamos el array filtrado para poder ordenarlos según sea el caso
+    localStorage.setItem('listaProductos', JSON.stringify(productosFiltrados));
+}
+
+function busquedaFiltro() {
+    let busqueda = barraBusqueda.value.toLowerCase(); // lo que el usuario ingresa en la barra de búsqueda
+    let productosBarraBusqueda = [];
+    let productos = JSON.parse(localStorage.getItem('productos'));
+
+    for (const producto of productos) {
+        // obtener los nombres y descripciones de cada producto y pasarlos a minúscula
+        nombre = producto.name.toLowerCase();
+        descripcion = producto.description.toLowerCase();
+        // Condicional que, con ayuda de indexOf compara lo que el usuario escribe en la barra, con la cadena de caracteres que sacamos de los productos;
+        //  y en caso que esté contenido, agrega el producto a un nuevo array que pasará por nuestro render de productos
+        if ((nombre.indexOf(busqueda) !== -1) || (descripcion.indexOf(busqueda) !== -1)) {
+            productosBarraBusqueda.push(producto);
+        }
+    }
+    showProductsList(productosBarraBusqueda);
+}
+
+precioAsc.addEventListener('click', ordenarProductos);
+precioDes.addEventListener('click', ordenarProductos);
+relevancia.addEventListener('click', ordenarProductos);
+botonRangoPrecio.addEventListener('click', filtrarProductos);
+// botón en forma de enlace que reinicia los filtros mostrando el array de productos originales
+limpiarFiltroRango.addEventListener('click', () => showProductsList(JSON.parse(localStorage.getItem('productos'))));
